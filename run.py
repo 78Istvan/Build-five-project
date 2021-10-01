926b85b27c2f91e030e3c71207df17b5a6af2253
78Istvan <istvan78juhasz@gmail.com>
2021-10-01 18:44:24 +0000

    Heroku deployment error fixes

--- run.py
+++ run.py
@@ -1,10 +1,5 @@
 import os
-from flask import (
-    Flask, flash, render_template,
-    redirect, request, session, url_for)
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask
 if os.path.exists("env.py"):
     import env
 
@@ -14,8 +9,6 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
-
-mongo = PyMongo(app)
 
 
 @app.route("/")
